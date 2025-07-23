7aaaffbcf40fa3b79b3d5703255b52acc0ba0fa1
Carlos Duarte <carlosduartec29@gmail.com>
2025-07-23 11:23:47 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Logbook</h1>
+      </header>
+      <div className="container">
+        <p>Welcome! Track your anime here.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
